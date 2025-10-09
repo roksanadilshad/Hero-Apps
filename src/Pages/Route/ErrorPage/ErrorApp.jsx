28dc6ec908cdbf1a126ed29476447cc1aa778543
Roksana Dilshad <roksanadilshad@gmail.com>
2025-10-09 16:36:52 +0600

    Error Image

--- src/Pages/Route/ErrorPage/ErrorApp.jsx
+++ src/Pages/Route/ErrorPage/ErrorApp.jsx
@@ -6,7 +6,7 @@ const ErrorApp = () => {
     return (
         <div>
              <div className='flex justify-center items-center flex-col'>
-           <img src="/src/assets/App-Error.png" alt="" />
+           <img src="https://i.ibb.co.com/hxVSZwpX/App-Error.png" alt="" />
            <h2 className='font-semibold text-5xl/[60px]'>Oops, App not found!</h2>
            <p className='text-xl/[32px] text-[#627382] m-2'>The page you are looking for is not available.</p>
            <button onClick={() => navigate(0) || navigate('/')} className='bg-gradient-to-r from-[#632EE3] to-[#9F62F2] btn text-white m-2'>Go Back</button>
